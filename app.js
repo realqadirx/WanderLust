af3660209ed834c808bf6d7e8c6f4213e05fcd81
Qadir <realqadirx@gmail.com>
2026-09-03 18:25:11 +0530

    Adding Cookies properties in the website

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+
 
 
 const listings = require("./routes/listing.js");
@@ -13,12 +15,12 @@ const reviews = require("./routes/review.js");
 let MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-.then(() => {
-    console.log("connected to DB");
-})
-.catch((err) => {
-    console.log(err);
-})
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 async function main() {
     await mongoose.connect(MONGO_URL);
@@ -30,6 +32,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.engine('ejs', ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
+
+const sessionOptions = {
+    secret: "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
+    
+}
+
+app.use(session(sessionOptions));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
