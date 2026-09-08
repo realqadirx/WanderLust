e75de5b1664dd4de38b116024d80afdbe43eae31
Qadir <realqadirx@gmail.com>
2026-09-08 18:05:33 +0530

    Integrate the flash for showing the Alerts

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const flash = require("connect-flash");
 
 
 
@@ -42,13 +43,20 @@ const sessionOptions = {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
     },
-    
-}
 
-app.use(session(sessionOptions));
+}
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
+});
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
 });
 
 app.use("/listings", listings);
