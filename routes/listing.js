7a4702cff37bdac138a25a221fc29d676fc3e4c1
Qadir <realqadirx@gmail.com>
2026-09-09 10:58:52 +0530

    Adding alerts in listing

--- routes/listing.js
+++ routes/listing.js
@@ -31,6 +31,10 @@ router.get("/new", (req, res) => {
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
+    if (!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        return res.redirect("/listings")
+    }
     res.render("listings/show.ejs", { listing });
 }));
 
@@ -41,6 +45,7 @@ router.post(
     wrapAsync(async (req, res, next) => {
         const newListing = new Listing(req.body.listing);
         await newListing.save();
+        req.flash("success", "New Listing Created!");
         res.redirect("/listings");
     })
 );
@@ -51,18 +56,23 @@ router.get(
     wrapAsync(async (req, res) => {
         let { id } = req.params;
         const listing = await Listing.findById(id);
+        if (!listing) {
+            req.flash("error", "Listing you requested for does not exist!");
+            return res.redirect("/listings")
+        }
         res.render("listings/edit.ejs", { listing });
     }));
 
 //Update Route
 router.put(
-    "/:id", 
+    "/:id",
     ValidateListing,
     wrapAsync(async (req, res) => {
-    let { id } = req.params;
-    await Listing.findByIdAndUpdate(id,{ ...req.body.listing });
-    res.redirect(`/listings/${id}`);
-}));
+        let { id } = req.params;
+        await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+        req.flash("success", "Listing Updated!");
+        res.redirect(`/listings/${id}`);
+    }));
 
 // Delete Route
 router.delete(
@@ -71,6 +81,7 @@ router.delete(
         let { id } = req.params;
         let deletedListing = await Listing.findByIdAndDelete(id);
         console.log(deletedListing);
+        req.flash("success", "Listing Deleted!");
         res.redirect("/listings");
     }));
 
