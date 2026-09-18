9cacefd8b3268e47a17ccafdbfc47ae6e2587eb3
Qadir <realqadirx@gmail.com>
2026-09-18 12:52:27 +0530

    Update review routes

--- routes/review.js
+++ routes/review.js
@@ -4,17 +4,19 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
 const Review = require("../models/review.js");
 const Listing = require("../models/listing.js");
-const {validateReview} = require("../middleware.js");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware.js");
 
 
 //Post Route
 router.post(
     "/",
+    isLoggedIn,
     validateReview,
     wrapAsync(async (req, res) => {
         let listing = await Listing.findById(req.params.id);
         let newReview = new Review(req.body.review);
-
+        newReview.author = req.user._id;
+        console.log(newReview);
         listing.reviews.push(newReview);
 
         await newReview.save();
@@ -26,6 +28,8 @@ router.post(
 //Delete Review Route
 router.delete(
     "/:reviewId",
+    isLoggedIn,
+    isReviewAuthor,
     wrapAsync(async (req, res) => {
         let { id, reviewId } = req.params;
 
